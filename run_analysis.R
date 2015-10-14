19070314112025c6aa2248cf972cb3834ce91bc2
Paul Hickman <paul_hickman@hotmail.com>
2015-10-14 16:26:27 +0800

    Last step remaining

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 #library(data.table)
 #library(dplyr)
 
-##setwd("c:/Users/phickma/Desktop/GitHub/UCI HAR Dataset")
+##setwd("c:/Users/phickma/Desktop/GitHub/GettingAndCleaningData")
 
 # 1. Merge the training and the test sets to create one data set.
 
@@ -45,5 +45,85 @@ step2Data <- allData[,grepl("Subject|Activity|mean\\(\\)|std\\(\\)", colnames(al
 
 
 # 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
+allActivities <- read.table("../UCI HAR Dataset/activity_labels.txt")
+# copy the associated Activity Name from allActivities to the data set (second column is Activity)
+step2Data$Activity <- allActivities[step2Data$Activity, 2]
+
+
+# 4. Appropriately labels the data set with descriptive variable names.
+# I have chosed to explicitly set the variable names, I could have used grep
+colnames(step2Data) <- list(
+  "Subject",
+  "Activity",
+  "timeBodyAccelerometer_Mean_X",
+  "timeBodyAccelerometer_Mean_Y",
+  "timeBodyAccelerometer_Mean_Z",
+  "timeBodyAccelerometer_Std_X",
+  "timeBodyAccelerometer_Std_Y",
+  "timeBodyAccelerometer_Std_Z",
+  "timeGravityAccelerometer_Mean_X",
+  "timeGravityAccelerometer_Mean_Y",
+  "timeGravityAccelerometer_Mean_Z",
+  "timeGravityAccelerometer_Std_X",
+  "timeGravityAccelerometer_Std_Y",
+  "timeGravityAccelerometer_Std_Z",
+  "timeBodyAccelerometerJerk_Mean_X",
+  "timeBodyAccelerometerJerk_Mean_Y",
+  "timeBodyAccelerometerJerk_Mean_Z",
+  "timeBodyAccelerometerJerk_Std_X",
+  "timeBodyAccelerometerJerk_Std_Y",
+  "timeBodyAccelerometerJerk_Std_Z",
+  "timeBodyGyroscope_Mean_X",
+  "timeBodyGyroscope_Mean_Y",
+  "timeBodyGyroscope_Mean_Z",
+  "timeBodyGyroscope_Std_X",
+  "timeBodyGyroscope_Std_Y",
+  "timeBodyGyroscope_Std_Z",
+  "timeBodyGyroscopeJerk_Mean_X",
+  "timeBodyGyroscopeJerk_Mean_Y",
+  "timeBodyGyroscopeJerk_Mean_Z",
+  "timeBodyGyroscopeJerk_Std_X",
+  "timeBodyGyroscopeJerk_Std_Y",
+  "timeBodyGyroscopeJerk_Std_Z",
+  "timeBodyAccelerometerMagnitude_Mean",
+  "timeBodyAccelerometerMagnitude_Std",
+  "timeGravityAccelerometerMagnitude_Mean",
+  "timeGravityAccelerometerMagnitude_Std",
+  "timeBodyAccelerometerJerkMagnitude_Mean",
+  "timeBodyAccelerometerJerkMagnitude_Std",
+  "timeBodyGyroscopeMagnitude_Mean",
+  "timeBodyGyroscopeMagnitude_Std",
+  "timeBodyGyroscopeJerkMagnitude_Mean",
+  "timeBodyGyroscopeJerkMagnitude_Std",
+  "frequencyBodyAccelerometer_Mean_X",
+  "frequencyBodyAccelerometer_Mean_Y",
+  "frequencyBodyAccelerometer_Mean_Z",
+  "frequencyBodyAccelerometer_Std_X",
+  "frequencyBodyAccelerometer_Std_Y",
+  "frequencyBodyAccelerometer_Std_Z",
+  "frequencyBodyAccelerometerJerk_Mean_X",
+  "frequencyBodyAccelerometerJerk_Mean_Y",
+  "frequencyBodyAccelerometerJerk_Mean_Z",
+  "frequencyBodyAccelerometerJerk_Std_X",
+  "frequencyBodyAccelerometerJerk_Std_Y",
+  "frequencyBodyAccelerometerJerk_Std_Z",
+  "frequencyBodyGyroscope_Mean_X",
+  "frequencyBodyGyroscope_Mean_Y",
+  "frequencyBodyGyroscope_Mean_Z",
+  "frequencyBodyGyroscope_Std_X",
+  "frequencyBodyGyroscope_Std_Y",
+  "frequencyBodyGyroscope_Std_Z",
+  "frequencyBodyAccelerometerMagnitude_Mean",
+  "frequencyBodyAccelerometerMagnitude_Std",
+  "frequencyBodyAccelerometerJerkMagnitude_Mean",
+  "frequencyBodyAccelerometerJerkMagnitude_Std",
+  "frequencyBodyGyroscopeMagnitude_Mean",
+  "frequencyBodyGyroscopeMagnitude_Std",
+  "frequencyBodyGyroscopeJerkMagnitude_Mean",
+  "frequencyBodyGyroscopeJerkMagnitude_Std"
+)
+
+
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
